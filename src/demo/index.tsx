cb5a915ed04ccad4d19cf51d800496a3f17956b1
Arek Czekajski <aczekajski@gmail.com>
2018-01-08 16:25:35 +0100

    missing formatting

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import * as ReactDOM from 'react-dom';
 
 const renderApp = () => {
-   ReactDOM.render(
-      <div>
-         No siema ;)
-      </div>
-      ,
-      document.getElementById('app'),
-   );
+    ReactDOM.render(
+        <div>
+            No siema ;)
+        </div>
+        ,
+        document.getElementById('app'),
+    );
 };
 
 if (module.hot) {
-   module.hot.accept('./*', () => {
-      renderApp();
-   });
+    module.hot.accept('./*', () => {
+        renderApp();
+    });
 }
