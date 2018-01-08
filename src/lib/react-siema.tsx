cde3b6c9bb09a63953deba0a5ae909afbdb3641e
Arek Czekajski <aczekajski@gmail.com>
2018-01-08 21:59:34 +0100

    first version (wip)

--- src/lib/react-siema.tsx
+++ src/lib/react-siema.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
-import SiemaBase from 'siema';
+import SiemaBase, { SiemaOptions as SiemaBaseOptions } from 'siema';
 
 interface SiemaProps {
-    transitionTime: number;
+    children: React.ReactNode[];
+    className?: string;
 }
 
-class Siema extends React.Component<SiemaProps> {
-    render() {
+class Siema extends React.Component<SiemaBaseOptions & { overflowHidden?: boolean } & SiemaProps> {
+    private siemaWrapper;
+    public siemaInstance;
+    private getSiemaWrapperRef = (element) => { this.siemaWrapper = element; };
+
+    public componentDidMount() {
+        const {
+            duration = 200,
+            easing = 'ease-out',
+            perPage = 1,
+            startIndex = 0,
+            draggable = true,
+            multipleDrag = true,
+            threshold = 20,
+            loop = false,
+            overflowHidden = true,
+            onInit = () => undefined,
+            onChange = () => undefined,
+        } = this.props;
+        this.siemaInstance = new SiemaBase({
+            selector: this.siemaWrapper,
+            duration,
+            easing,
+            perPage,
+            startIndex,
+            draggable,
+            multipleDrag,
+            threshold,
+            loop,
+            overflowHidden,
+            onInit,
+            onChange: () => onChange(this.siemaInstance.currentSlide),
+        } as any);
+    }
+
+    public render() {
         return (
-            <div>
-                Siemaneczko!
+            <div ref={this.getSiemaWrapperRef} className={this.props.className}>
+                {this.props.children}
             </div>
         );
     }
