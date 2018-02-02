a133a273b0b548a8cb30205e5e3b11e60243a888
x-kom Arkadiusz Czekajski <arkadiusz.czekajski@x-kom.org>
2018-02-02 18:03:24 +0100

    types fix

--- src/lib/siema-react.tsx
+++ src/lib/siema-react.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import SiemaBase, { SiemaOptions } from 'siema';
 
+export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
+export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+export type Overwrite<T, U> = Pick<T, Diff<keyof T, keyof U>> & U;
+
 export type SiemaProps = Overwrite<Omit<SiemaOptions, 'selector'>, {
     onChange?: (index: number) => void;
 }>;
