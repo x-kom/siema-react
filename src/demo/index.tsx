fc2ce41d4aeb6a3606b5388a98dd9363e29f4626
x-kom Wacław Łabuda <waclaw.labuda@x-kom.org>
2018-02-13 14:57:44 +0100

    cleanup code

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -57,12 +57,7 @@ const SmallSliderWrapper = styled.div`
     }
 `;
 
-const ExampleChildren = ({ img }: { img: any; }) => <div><img src={img} alt="Siema image" /></div>;
-
 class App extends React.Component {
-    public state = {
-        current: 0,
-    };
 
     private getSmallSliderRef = (element) => { this.smallSlider = element; };
     private getBigSliderRef = (element) => { this.bigSlider = element; };
@@ -72,15 +67,9 @@ class App extends React.Component {
 
     render() {
 
-        const { current } = this.state;
-
         return (
             <div>
                 No siema ;)
-                <BigSlider clickable={true} overflowHidden={false}>
-                    <ExampleChildren img={'one'} />
-                    <ExampleChildren img={'two'} />
-                </BigSlider>
                 <BigSlider>
                     <div><img src="http://via.placeholder.com/350x150/FFC0CB?text=1" alt="Siema image" /></div>
                     <div><img src="http://via.placeholder.com/350x150/ADD8E6?text=2" alt="Siema image" /></div>
@@ -117,8 +106,8 @@ class App extends React.Component {
                     <div><img src="http://via.placeholder.com/350x150/FFC0CB?text=9" alt="Siema image" /></div>
                 </TripleSlider>
                 <hr style={{ clear: 'both' }} />
-                <button onClick={() => { this.bigSlider.prev(); this.setState({ current: 0 }) }}>prev</button>
-                <button onClick={() => { this.bigSlider.next(); this.setState({ current: 1 }) }}>next</button>
+                <button onClick={() => { this.bigSlider.prev(); }}>prev</button>
+                <button onClick={() => { this.bigSlider.next(); }}>next</button>
                 <BigSlider duration={100} innerRef={this.getBigSliderRef} onChange={(index: number) => { this.smallSlider.goTo(index); }}>
                     <div><img src="http://via.placeholder.com/350x150/FFC0CB?text=1" alt="Siema image" /></div>
                     <div><img src="http://via.placeholder.com/350x150/ADD8E6?text=2" alt="Siema image" /></div>
@@ -131,10 +120,10 @@ class App extends React.Component {
                     <div><img src="http://via.placeholder.com/350x150/FFC0CB?text=9" alt="Siema image" /></div>
                 </BigSlider>
                 <SmallSliderWrapper>
-                    <SmallSlider duration={100} innerRef={this.getSmallSliderRef} onChange={(index: number) => { this.bigSlider.goTo(index); this.setState({ current: index }) }} overflowHidden={false} clickable={false}>
-                        <div style={{ border: current === 0 ? '1px solid red' : '1px solid green' }}><img src="http://via.placeholder.com/350x150/FFC0CB?text=1" alt="Siema image" /></div>
-                        <div style={{ border: current === 1 ? '1px solid red' : '1px solid green' }}><img src="http://via.placeholder.com/350x150/ADD8E6?text=2" alt="Siema image" /></div>
-                        <div style={{ border: current === 2 ? '1px solid red' : '1px solid green' }}><img src="http://via.placeholder.com/350x150/FFC0CB?text=3" alt="Siema image" /></div>
+                    <SmallSlider duration={100} innerRef={this.getSmallSliderRef} onChange={(index: number) => { this.bigSlider.goTo(index); }} overflowHidden={false} clickable={true}>
+                        <div><img src="http://via.placeholder.com/350x150/FFC0CB?text=1" alt="Siema image" /></div>
+                        <div><img src="http://via.placeholder.com/350x150/ADD8E6?text=2" alt="Siema image" /></div>
+                        <div><img src="http://via.placeholder.com/350x150/FFC0CB?text=3" alt="Siema image" /></div>
                         <div><img src="http://via.placeholder.com/350x150/ADD8E6?text=4" alt="Siema image" /></div>
                         <div><img src="http://via.placeholder.com/350x150/FFC0CB?text=5" alt="Siema image" /></div>
                         <div><img src="http://via.placeholder.com/350x150/ADD8E6?text=6" alt="Siema image" /></div>
