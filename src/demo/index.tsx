57d5c42a7f550adc85984542e39d95105cbe0df9
x-kom Arkadiusz Czekajski <arkadiusz.czekajski@x-kom.org>
2018-06-25 19:52:41 +0200

    extended demo

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -57,7 +57,14 @@ const SmallSliderWrapper = styled.div`
     }
 `;
 
-class App extends React.Component<{}, { slides: number; images: string[] }> {
+const SingleSlide = styled.div`
+    border: 1px solid red;
+    border-radius: 8px;
+    overflow: hidden;
+    line-height: 0;
+`;
+
+class App extends React.Component<{}, { slides: number; images: string[]; mounted: boolean }> {
 
     state = {
         slides: 5,
@@ -72,6 +79,7 @@ class App extends React.Component<{}, { slides: number; images: string[] }> {
             'http://via.placeholder.com/350x150/ADD8E6?text=8',
             'http://via.placeholder.com/350x150/FFC0CB?text=9',
         ],
+        mounted: true,
     };
 
     private getSmallSliderRef = (element) => { this.smallSlider = element; };
@@ -79,6 +87,10 @@ class App extends React.Component<{}, { slides: number; images: string[] }> {
 
     private smallSlider;
     private bigSlider;
+
+    toggleMounted = () => {
+        this.setState((state) => ({ mounted: !state.mounted }));
+    }
 
     moreSlides = () => {
         this.changeNumberOfSlides(1);
@@ -111,7 +123,7 @@ class App extends React.Component<{}, { slides: number; images: string[] }> {
                 <button onClick={this.lessSlides}>less slides</button>
                 <button onClick={this.shuffleSlides}>shuffle slides</button>
                 <BigSlider onChange={(index) => { console.log('onchange', index); }}>
-                    {this.state.images.slice(0, this.state.slides).map((src) => <div key={src}><img src={src} alt="Siema image" /></div>)}
+                    {this.state.images.slice(0, this.state.slides).map((src) => <SingleSlide key={src}><img src={src} alt="Siema image" /></SingleSlide>)}
                 </BigSlider>
                 <hr />
                 Multiple visible slides
@@ -166,6 +178,11 @@ class App extends React.Component<{}, { slides: number; images: string[] }> {
                         <div><img src="http://via.placeholder.com/350x150/FFC0CB?text=9" alt="Siema image" /></div>
                     </SmallSlider>
                 </SmallSliderWrapper>
+                <hr />
+                Unmounting example <button onClick={this.toggleMounted}>mount/unmount</button>
+                {this.state.mounted && <BigSlider>
+                    {this.state.images.map((src) => <div key={src}><img src={src} alt="Siema image" /></div>)}
+                </BigSlider>}
             </div>
         );
     }
